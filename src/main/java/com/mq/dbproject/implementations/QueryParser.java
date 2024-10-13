d6beca7c2b748c464cf73ceae6d1707677fc3a95
maha <mahaquarqout123@gmail.com>
2024-10-13 21:25:07 +0300

    Query

--- src/main/java/com/mq/dbproject/implementations/QueryParser.java
+++ src/main/java/com/mq/dbproject/implementations/QueryParser.java
@@ -1,11 +1,14 @@
 package main.java.com.mq.dbproject.implementations;
 
-import main.java.com.mq.dbproject.interfaces.IQueryParser;
+import  main.java.com.mq.dbproject.interfaces.IQueryParser;
+
+// import java.util.HashMap;
+// import java.util.Map;
 
 public class QueryParser implements IQueryParser {
     @Override
     public String[] parse(String query) {
-        // A simple parse logic 
+        // A simple parse logic - this can be expanded later
         return query.split(" ");
     }
 }
--- src/main/java/com/mq/dbproject/interfaces/IQueryParser.java
+++ src/main/java/com/mq/dbproject/interfaces/IQueryParser.java
@@ -3,3 +3,7 @@ package main.java.com.mq.dbproject.interfaces;
 public interface IQueryParser {
     String[] parse(String query);
 }
+
+// public interface IQueryParser {
+//     void parse(String query); // Ensure the return type is void
+// }
